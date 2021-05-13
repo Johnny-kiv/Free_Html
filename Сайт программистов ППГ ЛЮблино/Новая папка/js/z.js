97c077f3011dbcb7f8cfcc7209e1087576142fee
Иван <77494079+Johnny-kiv@users.noreply.github.com>
2021-05-13 22:27:54 +0300

    Update z.js

--- Сайт программистов ППГ ЛЮблино/Новая папка/js/z.js
+++ Сайт программистов ППГ ЛЮблино/Новая папка/js/z.js
@@ -5,32 +5,26 @@ function checkForm(el){
     var repass = document.getElementById('RePass').value;
     var fail="";
     if(name==""||pass==""||email==""||repass=="")
-      fail="Заполните все поля!!!";
+      fail="Заполните все поля.";
     else if(name.length<=1 || name.length>50)
       fail="Введите коректное имя.";
-    else if(pass<8)
+    else if(pass.length<8)
       fail="Пароль должен быть не меньше 8 символов.";
     else if(pass != repass)
-      fail="Пароли не должны совпадать.";
+      fail="Пароли должны совпадать.";
     else if(pass.split("$").length>1)
-      fail="Символ $ в пароле дожен быть не больше 1-го раза.";
+      fail="Знак $ дожен быть не больше 1-го раза.";
     else if(pass.split("!").length>1)
-      fail="Символ ! в пароле быть не больше 2-х раза.";
+      fail="Знак ! дожен быть не больше 2-х раза.";
     else if(pass.split("@").length>1)
-      fail="Символ @ в пароле дожен быть не больше 1-го раза.";
+      fail="Знак @ дожен быть не больше 1-го раза.";
     else if(pass.split("#").length>1)
-      fail="Символ # d пароле дожен быть не больше 1-го раза.";
-    else if(isCyrillic(pass)==true)
-      fail="Язык должен быть английским.";
-    else if(email.split("@")length==0)
-      fail="Адрес почты должен иметь символ @";
-    else if(email.split("@")length>1)
-      fail="Адрес почты должен иметь символ @ не больше 1-го";
+      fail="Знак # дожен быть не больше 1-го раза.";
     if (fail !=""){
       document.getElementById('error').innerHTML=fail;
       return false;
     }else {
-      alert("Все данные коректное.");
+      alert("Все данные заполнены коректное.");
       return true;
     }
 }
