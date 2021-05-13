23b8600904ad806097741a65e8258a2e7e692a72
Иван <77494079+Johnny-kiv@users.noreply.github.com>
2021-05-13 21:16:26 +0300

    Update z.js

--- Сайт программистов ППГ ЛЮблино/Новая папка/js/z.js
+++ Сайт программистов ППГ ЛЮблино/Новая папка/js/z.js
@@ -7,8 +7,20 @@ function checkForm(el){
     if(name==""||pass==""||email==""||repass=="")
       fail="Заполните все поля!!!";
     else if(name.length<=1 || name.length>50)
-      fail="Введите корекное имя!!!";
+      fail="Введите корекное имя/";
     else if(pass<8)
-      fail="Пароль должен быть не меньше 8 символов!!!"
+      fail="Пароль должен быть не меньше 8 символов.";
+    else if(pass != repass)
+      fail="Пароли не должны совпадать.";
+    else if(pass.split("$").length>1)
+      fail="Знак $ дожен быть не больше 1-го раза.";
+    else if(pass.split("!").length>1)
+      fail="Знак ! дожен быть не больше 2-х раза.";
+    else if(pass.split("@").length>1)
+      fail="Знак @ дожен быть не больше 1-го раза.";
+    else if(pass.split("#").length>1)
+      fail="Знак # дожен быть не больше 1-го раза.";
+    else if(isCyrillic(pass)==true)
+      fail="Язык должен быть английским.";
     return false;
 }
