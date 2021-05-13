a6679a123a00bd942a8c7afd4612b6498261f319
Иван <77494079+Johnny-kiv@users.noreply.github.com>
2021-05-13 19:00:33 +0300

    р

--- Сайт программистов ППГ ЛЮблино/Новая папка/js/z.js
+++ Сайт программистов ППГ ЛЮблино/Новая папка/js/z.js
@@ -1,12 +1,1 @@
-var stroka = "word";
-switch (stroka) {
-  case "r":
-  console.log("r");
-    break;
-  case "b":
-  console.log("b");
-    break;
-  case "word":
-  console.log("word");
-    break;
-}
+
